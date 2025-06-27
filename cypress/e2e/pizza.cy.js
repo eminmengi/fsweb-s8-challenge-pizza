8bb99f8fa8b6b48d2103581ed5af56de25373aee
Emin Mengi <eminmengi@gmail.com>
2025-06-27 14:42:25 +0300

    testler güncellendi

--- cypress/e2e/pizza.cy.js
+++ cypress/e2e/pizza.cy.js
@@ -8,16 +8,16 @@ describe('Pizza Formu', () => {
   });
 
   it('Malzeme seçimi yapılabilmeli', () => {
-    cy.get("input[type=checkbox]").check(["Pepperoni", "Sosis", "Sarımsak", "Jalapeno"])
+    cy.get("input[type=checkbox]").check(["Ananas", "Sosis", "Biber", "Jalapeno"], { force: true });
   });
 
   it('Boyut seçimi yapılabilmeli', () => {
-    cy.get("input[type=radio]").check("Büyük");
+    cy.contains('label.size-option', 'L').click();
+    cy.get("input[value='Büyük']").should('be.checked');
   });
   
   it('Hamur seçimi yapılabilmeli', () => {
     cy.get('select[name="dough"]').select('İnce').should('have.value', 'İnce');
-    cy.get('select[name="dough"]').select('Normal').should('have.value', 'Normal');
     cy.get('select[name="dough"]').select('Kalın').should('have.value', 'Kalın');
   });
 
@@ -27,11 +27,12 @@ describe('Pizza Formu', () => {
 
   it('Form gönderilebilmeli', () => {
     cy.get('input[name="name"]').type('Emin');
-    cy.get("input[value=Orta]").click();
+    cy.contains('label.size-option', 'L').click();
     cy.get("select[name=dough]").select('Kalın');
-    cy.get("input[type=checkbox]").check(["Pepperoni", "Sosis", "Sarımsak", "Jalapeno"]);
+    cy.get("input[type=checkbox]").check(["Ananas", "Sosis", "Biber", "Jalapeno"], { force: true });
     cy.get('textarea[name="note"]').type('Az pişmiş olsun');
     cy.get('button[type="submit"]').click();
-    cy.contains('SİPARİŞİNİZ ALINDI!').should('exist');
+    cy.url().should('include', '/success');
+    cy.contains('SİPARİŞ ALINDI').should('exist');
   });
 })
